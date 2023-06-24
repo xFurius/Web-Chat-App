4ee9655c95f99a7baf76e03a6948e81b3b47c4fb
Mateusz Pszczoła <matip03211@gmail.com>
2023-06-24 22:23:20 +0200

    refactor

--- src/main/java/com/example/webChatApp/security/AuthRequest.java
+++ src/main/java/com/example/webChatApp/security/AuthRequest.java
@@ -1,8 +1,5 @@
 package com.example.webChatApp.security;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,9 +12,6 @@ import lombok.ToString;
 @Getter
 @ToString
 public class AuthRequest{
-    @NotEmpty
-    @Email
     private String email;
-    @NotEmpty
     private String password;
 }
